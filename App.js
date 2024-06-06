35a4508ddacb8fe2e84ea8a22e151f996f534965
shahzaib <shahzaib@gmail.com>
2024-06-06 11:07:14 +0530

    ep-1

---
+++ App.js
@@ -0,0 +1,4 @@
+const heading=React.createElement('h1', {}, "Hello React From Javascript")
+const root=ReactDOM.createRoot(document.getElementById("root")); 
+root.render(heading)
+console.log(heading);
